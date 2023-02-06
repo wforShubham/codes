a2e49b6e950f00355c7f65157822a4eae70d35cd
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2023-02-06 20:19:34 +0530

    mandatory update

--- first_chat_bott.cpp
+++ first_chat_bott.cpp
@@ -1,24 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     string a;
-    cout<<"how do you feel today?"<<endl;
-    cin>>a;
-    if(a=="good"){
-        cout<<"good to know that"<<endl;
-        }
-        else if(a=="not good"){
-            cout<<"felt bad for you but imma machine can't help you"<<endl;
-        }
-        else if(a=="feeling sick"){
-            cout<<"you must vist to doctor"<<endl;
-        }
-        else if(a=="hungry"){
-            cout<<"go to your mom"<<endl;
-        }
-        else if(a=="i want a gf"||"want a gf"||"want to date someone"){
-            cout<<"what am i  suppose to do?"<<endl;
-        }
+    cout << "how do you feel today?" << endl;
+    cin >> a;
+    if (a == "good")
+    {
+        cout << "good to know that" << endl;
+    }
+    else if (a == "not good")
+    {
+        cout << "felt bad for you but imma machine can't help you" << endl;
+    }
+    else if (a == "feeling sick")
+    {
+        cout << "you must vist to doctor" << endl;
+    }
+    else if (a == "hungry")
+    {
+        cout << "go to your mom" << endl;
+    }
+    else if (a == "i want a gf" || "want a gf" || "want to date someone")
+    {
+        cout << "what am i  suppose to do?" << endl;
+    }
+    else if (a == "shubham bkl" || "bkl is shubham")
+        cout << "chup bkl, tu bkl";
 
-        return 0;
+    return 0;
 }
